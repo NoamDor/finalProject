b203b5949b0d7f613217fa56f7e16e1eb76aa064
Noam Dor <noamdavidpur@gmail.com>
2020-04-01 20:23:33 +0300

    noam's connection string

--- finalProject/finalProject/Migrations/Configuration.cs
+++ finalProject/finalProject/Migrations/Configuration.cs
@@ -10,6 +10,7 @@ namespace finalProject.Migrations
         public Configuration()
         {
             AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
         }
 
         protected override void Seed(finalProject.StoreContext context)
